90b8c02c49bd2718a598e1e35794900b8ba29aea
Breno AllenCS <brenoallencs@gmail.com>
2023-06-02 16:46:09 -0300

    CrossOrigin for web

--- src/main/java/com/breallencs/mytripyapi/core/jwt/JwtRestController.java
+++ src/main/java/com/breallencs/mytripyapi/core/jwt/JwtRestController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ import com.breallencs.mytripyapi.modules.user.User;
 import com.breallencs.mytripyapi.modules.user.UserRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class JwtRestController {
   
   @Value("${jwt.http.request.header}")
